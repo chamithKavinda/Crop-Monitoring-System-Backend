3fb4a9fc8731a2e5eca3502f6087222443c70a95
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-05 22:25:37 +0530

    AppUtil , Mapping classes added

---
+++ src/main/java/org/example/cropmonitoringsystem/util/AppUtil.java
@@ -0,0 +1,7 @@
+package org.example.cropmonitoringsystem.util;
+
+import java.util.UUID;
+
+public class AppUtil {
+    public static String createVehicleCode(){return "Vehicle-"+ UUID.randomUUID();}
+}
---
+++ src/main/java/org/example/cropmonitoringsystem/util/Mapping.java
@@ -0,0 +1,28 @@
+package org.example.cropmonitoringsystem.util;
+
+import org.example.cropmonitoringsystem.dto.impl.VehicleDTO;
+import org.example.cropmonitoringsystem.entity.VehicleEntity;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Component
+public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    //VehicleEntity and DTO
+    public VehicleDTO convertToDTO(VehicleDTO vehicle){
+        return modelMapper.map(vehicle, VehicleDTO.class);
+    }
+    public VehicleEntity convertToEntity(VehicleDTO dto){
+        return modelMapper.map(dto, VehicleEntity.class);
+    }
+    public List<VehicleDTO> convertToDTO(List<VehicleEntity> vehicles){
+        return modelMapper.map(vehicles, new TypeToken<List<VehicleDTO>>(){}.getType());
+    }
+}
