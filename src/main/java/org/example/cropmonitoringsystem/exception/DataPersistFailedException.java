266987c6e5c8b1d8ad80c1dab0089075fb1e7d09
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-05 22:26:35 +0530

    DataPersistFailedException added

---
+++ src/main/java/org/example/cropmonitoringsystem/exception/DataPersistFailedException.java
@@ -0,0 +1,10 @@
+package org.example.cropmonitoringsystem.exception;
+
+public class DataPersistFailedException extends RuntimeException{
+    public DataPersistFailedException() {
+    }
+    public DataPersistFailedException(String message) {
+    }
+    public DataPersistFailedException(String message, Throwable cause) {
+    }
+}
