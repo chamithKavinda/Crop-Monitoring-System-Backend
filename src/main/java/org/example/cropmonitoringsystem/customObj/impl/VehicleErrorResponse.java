1050443aab7c4eb62235375ff28cef6becab19c3
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-05 20:05:53 +0530

    VehicleErrorResponse, VehicleResponse custom objects added

---
+++ src/main/java/org/example/cropmonitoringsystem/customObj/impl/VehicleErrorResponse.java
@@ -0,0 +1,14 @@
+package org.example.cropmonitoringsystem.customObj.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.cropmonitoringsystem.customObj.VehicleResponse;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class VehicleErrorResponse implements VehicleResponse, Serializable {
+}
