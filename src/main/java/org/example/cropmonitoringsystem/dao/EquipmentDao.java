0896db6c7f997aba13639a0a7f2a7e346d29f732
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-06 16:38:54 +0530

    applied EquipmentDao

---
+++ src/main/java/org/example/cropmonitoringsystem/dao/EquipmentDao.java
@@ -0,0 +1,9 @@
+package org.example.cropmonitoringsystem.dao;
+
+import org.example.cropmonitoringsystem.entity.EquipmentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipmentDao extends JpaRepository<EquipmentEntity,String> {
+}
