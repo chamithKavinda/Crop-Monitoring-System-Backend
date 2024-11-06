b78510fa93f640c25070c433622a4a00b5558cdf
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-06 16:54:59 +0530

    EquipmentDTO added

---
+++ src/main/java/org/example/cropmonitoringsystem/dto/impl/EquipmentDTO.java
@@ -0,0 +1,18 @@
+package org.example.cropmonitoringsystem.dto.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.cropmonitoringsystem.customObj.EquipmentResponse;
+import org.example.cropmonitoringsystem.dto.SuperDTO;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class EquipmentDTO implements SuperDTO, EquipmentResponse {
+    private String equipmentId;
+    private String name;
+    private String type;
+    private String status;
+    private String staffId;
+    private String fieldCode;
+}
