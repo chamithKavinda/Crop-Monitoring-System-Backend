c53e8612e887792dad6e5b58f06061bc4836d563
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-08 18:50:50 +0530

    CropEntity, CropDTO implemented

---
+++ src/main/java/org/example/cropmonitoringsystem/dto/impl/CropDTO.java
@@ -0,0 +1,19 @@
+package org.example.cropmonitoringsystem.dto.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.cropmonitoringsystem.customObj.CropResponse;
+import org.example.cropmonitoringsystem.dto.SuperDTO;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CropDTO implements SuperDTO, CropResponse {
+    private String cropCode;
+    private String cropCommonName;
+    private String cropScientificName;
+    private String cropImage;
+    private String category;
+    private String cropSeason;
+    private String fieldCode;
+}
