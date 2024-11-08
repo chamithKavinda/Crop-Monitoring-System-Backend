edb862ac2ea97e94c4fbf2caae8ac28882daa4af
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-08 20:14:38 +0530

    CropNotFound exception added

---
+++ src/main/java/org/example/cropmonitoringsystem/exception/CropNotFound.java
@@ -0,0 +1,7 @@
+package org.example.cropmonitoringsystem.exception;
+
+public class CropNotFound extends RuntimeException{
+    public CropNotFound(){}
+    public CropNotFound(String message){}
+    public CropNotFound(String message, Throwable cause){}
+}
