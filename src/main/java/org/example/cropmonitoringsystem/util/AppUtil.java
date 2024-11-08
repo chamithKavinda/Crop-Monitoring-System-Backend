fbe236668a83bafdd2d1bd76d6feb41d1d0d18b6
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-08 20:15:27 +0530

    covert image toBase64 format

--- src/main/java/org/example/cropmonitoringsystem/util/AppUtil.java
+++ src/main/java/org/example/cropmonitoringsystem/util/AppUtil.java
@@ -1,9 +1,16 @@
 package org.example.cropmonitoringsystem.util;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
 import java.util.UUID;
 
 public class AppUtil {
     public static String createVehicleCode(){return "Vehicle-"+ UUID.randomUUID();}
     public static String createEquipmentId(){return "Equipment-"+ UUID.randomUUID();}
     public static String createCropId(){return "Crop-"+ UUID.randomUUID();}
+    public static String toBase64CropImage(MultipartFile CropImage) throws IOException {
+        return Base64.getEncoder().encodeToString(CropImage.getBytes());
+    }
 }
