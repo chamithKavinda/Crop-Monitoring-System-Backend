bdb21738a49d210af301f92045f52ad639e83ab9
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-08 19:06:11 +0530

    Generate Crop Code implemented

--- src/main/java/org/example/cropmonitoringsystem/util/AppUtil.java
+++ src/main/java/org/example/cropmonitoringsystem/util/AppUtil.java
@@ -5,4 +5,5 @@ import java.util.UUID;
 public class AppUtil {
     public static String createVehicleCode(){return "Vehicle-"+ UUID.randomUUID();}
     public static String createEquipmentId(){return "Equipment-"+ UUID.randomUUID();}
+    public static String createCropId(){return "Crop-"+ UUID.randomUUID();}
 }
