8ed366e0e2f3bacf8e60396c86b5469e2cce4011
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-10 08:44:43 +0530

    FieldNotFound Exception added

---
+++ src/main/java/org/example/cropmonitoringsystem/exception/FieldNotFound.java
@@ -0,0 +1,7 @@
+package org.example.cropmonitoringsystem.exception;
+
+public class FieldNotFound extends RuntimeException{
+    public FieldNotFound(){}
+    public FieldNotFound(String message){}
+    public FieldNotFound(String message, Throwable cause){}
+}
