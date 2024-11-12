951a643ee53f86231f7209d5e92983917c0e03a9
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-12 20:02:34 +0530

    StaffNotFound exception added

---
+++ src/main/java/org/example/cropmonitoringsystem/exception/StaffNotFound.java
@@ -0,0 +1,7 @@
+package org.example.cropmonitoringsystem.exception;
+
+public class StaffNotFound extends RuntimeException {
+    public StaffNotFound(){}
+    public StaffNotFound(String message){}
+    public StaffNotFound(String message, Throwable cause){}
+}
