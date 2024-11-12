684626bd0c58eb2b9a93fcc6478c4f0c03fac256
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-12 15:31:17 +0530

    Generate FieldId and , convert images tobase64 bytes

--- src/main/java/org/example/cropmonitoringsystem/util/AppUtil.java
+++ src/main/java/org/example/cropmonitoringsystem/util/AppUtil.java
@@ -10,11 +10,28 @@ public class AppUtil {
     public static String createVehicleCode(){return "Vehicle-"+ UUID.randomUUID();}
     public static String createEquipmentId(){return "Equipment-"+ UUID.randomUUID();}
     public static String createCropId(){return "Crop-"+ UUID.randomUUID();}
+    public static String createFieldId(){return "Field-"+ UUID.randomUUID();}
+
     public static String toBase64CropImage(MultipartFile cropImage) throws IOException {
         if (cropImage == null || cropImage.isEmpty()) {
             return null;
         }
         return Base64.getEncoder().encodeToString(cropImage.getBytes());
     }
+
+    public static String toBase64FieldImage1(MultipartFile fieldImage1) throws IOException{
+        if (fieldImage1 == null || fieldImage1.isEmpty()){
+            return null;
+        }
+        return Base64.getEncoder().encodeToString(fieldImage1.getBytes());
+    }
+
+    public static String toBase64FieldImage2(MultipartFile fieldImage2) throws IOException{
+        if (fieldImage2 == null || fieldImage2.isEmpty()){
+            return null;
+        }
+        return Base64.getEncoder().encodeToString(fieldImage2.getBytes());
+    }
+
 
 }
