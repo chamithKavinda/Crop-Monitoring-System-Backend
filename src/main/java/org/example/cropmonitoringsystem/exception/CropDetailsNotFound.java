575acf013c7428a57b8afc30fb604600ac56cb90
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-13 23:09:32 +0530

    CropDetailsNotFound Exception added

---
+++ src/main/java/org/example/cropmonitoringsystem/exception/CropDetailsNotFound.java
@@ -0,0 +1,7 @@
+package org.example.cropmonitoringsystem.exception;
+
+public class CropDetailsNotFound extends RuntimeException{
+    public CropDetailsNotFound(){}
+    public CropDetailsNotFound(String message){}
+    public CropDetailsNotFound(String message, Throwable cause){}
+}
