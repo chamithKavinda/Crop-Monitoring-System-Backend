96fa3d4c36c0b3be726d0f3f366381e762e39764
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-18 19:50:12 +0530

    Add controllers for auth request and create model for JWT response

---
+++ src/main/java/org/example/cropmonitoringsystem/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package org.example.cropmonitoringsystem.config;
+
+public class SecurityConfig {
+}
---
+++ src/main/java/org/example/cropmonitoringsystem/jwtModels/JWTResponse.java
@@ -0,0 +1,12 @@
+package org.example.cropmonitoringsystem.jwtModels;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class JWTResponse {
+    private String token;
+}
