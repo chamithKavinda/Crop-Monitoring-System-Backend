a4cef08887c81b34a251b7143f578d89ea073836
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2024-11-18 19:55:10 +0530

    Add endpoint for signIn and create model for SignIn

--- src/main/java/org/example/cropmonitoringsystem/controller/AuthController.java
+++ src/main/java/org/example/cropmonitoringsystem/controller/AuthController.java
@@ -1,5 +1,10 @@
 package org.example.cropmonitoringsystem.controller;
 
+import org.example.cropmonitoringsystem.jwtModels.JWTResponse;
+import org.example.cropmonitoringsystem.jwtModels.SignIn;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/auth")
 
 public class AuthController {
+    @PostMapping(value = "signin")
+    public ResponseEntity<JWTResponse> signIn(@RequestBody SignIn signIn) {
+        return null;
+    }
 }
